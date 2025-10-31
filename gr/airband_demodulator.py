88a5172abcf33844270600011fa7114b4c3b5300
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-10-31 19:07:19 +0900

    feat: ✨ better demodulator

--- gr/airband_demodulator.py
+++ gr/airband_demodulator.py
@@ -9,6 +9,7 @@
 # Author: Kentaro1043
 # GNU Radio version: 3.10.12.0
 
+from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import filter
 from gnuradio.filter import firdes
@@ -35,26 +36,22 @@ class airband_demodulator(gr.top_block):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 2.4e6
+        self.samp_rate = samp_rate = 2.56e6
         self.output_path = output_path = "/tmp/gnuradio_audio.pcm"
         self.freq = freq = 128.8e6
+        self.center_freq = center_freq = 129.0e6
 
         ##################################################
         # Blocks
         ##################################################
 
-        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
-                interpolation=1,
-                decimation=5,
-                taps=[],
-                fractional_bw=0)
         self.osmosdr_source_0 = osmosdr.source(
             args="numchan=" + str(1) + " " + "rtl=0"
         )
         self.osmosdr_source_0.set_time_unknown_pps(osmosdr.time_spec_t())
         self.osmosdr_source_0.set_sample_rate(samp_rate)
-        self.osmosdr_source_0.set_center_freq(freq, 0)
-        self.osmosdr_source_0.set_freq_corr(25, 0)
+        self.osmosdr_source_0.set_center_freq(center_freq, 0)
+        self.osmosdr_source_0.set_freq_corr(0, 0)
         self.osmosdr_source_0.set_dc_offset_mode(0, 0)
         self.osmosdr_source_0.set_iq_balance_mode(0, 0)
         self.osmosdr_source_0.set_gain_mode(True, 0)
@@ -64,30 +61,38 @@ class airband_demodulator(gr.top_block):
         self.osmosdr_source_0.set_antenna('', 0)
         self.osmosdr_source_0.set_bandwidth(0, 0)
         self.low_pass_filter_0_0 = filter.fir_filter_ccf(
-            10,
+            80,
             firdes.low_pass(
                 1,
                 samp_rate,
-                10e3,
-                5e3,
+                8e3,
+                2e3,
                 window.WIN_HAMMING,
                 6.76))
-        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(32767.0)
-        self.blocks_float_to_short_0 = blocks.float_to_short(1, 1)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_short*1, output_path, True)
+        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(0.5)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_float*1, output_path, True)
         self.blocks_file_sink_0.set_unbuffered(True)
-        self.blocks_complex_to_mag_0 = blocks.complex_to_mag(1)
+        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, (center_freq - freq), 1, 0, 0)
+        self.analog_am_demod_cf_0 = analog.am_demod_cf(
+        	channel_rate=(samp_rate / 80),
+        	audio_decim=1,
+        	audio_pass=5e3,
+        	audio_stop=5.5e3,
+        )
+        self.analog_agc_xx_0 = analog.agc_cc((1e-4), 1.0, 1.0, 65536)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_complex_to_mag_0, 0), (self.blocks_multiply_const_vxx_0, 0))
-        self.connect((self.blocks_float_to_short_0, 0), (self.blocks_file_sink_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_float_to_short_0, 0))
-        self.connect((self.low_pass_filter_0_0, 0), (self.rational_resampler_xxx_0, 0))
-        self.connect((self.osmosdr_source_0, 0), (self.low_pass_filter_0_0, 0))
-        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_complex_to_mag_0, 0))
+        self.connect((self.analog_agc_xx_0, 0), (self.analog_am_demod_cf_0, 0))
+        self.connect((self.analog_am_demod_cf_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.low_pass_filter_0_0, 0))
+        self.connect((self.low_pass_filter_0_0, 0), (self.analog_agc_xx_0, 0))
+        self.connect((self.osmosdr_source_0, 0), (self.blocks_multiply_xx_0, 0))
 
 
     def get_samp_rate(self):
@@ -95,7 +100,8 @@ class airband_demodulator(gr.top_block):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.low_pass_filter_0_0.set_taps(firdes.low_pass(1, self.samp_rate, 10e3, 5e3, window.WIN_HAMMING, 6.76))
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.low_pass_filter_0_0.set_taps(firdes.low_pass(1, self.samp_rate, 8e3, 2e3, window.WIN_HAMMING, 6.76))
         self.osmosdr_source_0.set_sample_rate(self.samp_rate)
 
     def get_output_path(self):
@@ -110,7 +116,15 @@ class airband_demodulator(gr.top_block):
 
     def set_freq(self, freq):
         self.freq = freq
-        self.osmosdr_source_0.set_center_freq(self.freq, 0)
+        self.analog_sig_source_x_0.set_frequency((self.center_freq - self.freq))
+
+    def get_center_freq(self):
+        return self.center_freq
+
+    def set_center_freq(self, center_freq):
+        self.center_freq = center_freq
+        self.analog_sig_source_x_0.set_frequency((self.center_freq - self.freq))
+        self.osmosdr_source_0.set_center_freq(self.center_freq, 0)
 
 
 
